a05cf25f59107f975154a2f59a2a7fe155ecccef
Rafael-M1 <rafael.muzzi2@gmail.com>
2021-11-21 14:33:19 -0400

    Pagination of category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,16 +24,17 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	/*
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		/*
-		 * List<CategoryDTO> listDto = new ArrayList<>(); for (Category cat : list) {
-		 * listDto.add(new CategoryDTO(cat)); } return listDto;
-		 */
-	}
+		
+		List<CategoryDTO> listDto = new ArrayList<>(); for (Category cat : list) {
+		listDto.add(new CategoryDTO(cat)); } return listDto; 
+	}  
+	*/
 
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
@@ -72,6 +73,12 @@ public class CategoryService {
 		catch(DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity Violation");
 		}
+	}
+
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 	
 }
